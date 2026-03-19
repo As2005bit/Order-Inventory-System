edc6fecc6f72d0445598a99ece23b06f0902b24a
Atul Sharma <atul830661@gmail.com>
2026-03-20 02:43:47 +0530

    added sample data for testing

---
+++ sql/sample_data.sql
@@ -0,0 +1,14 @@
+-- Sample data used for testing
+
+INSERT INTO customers (cust_name)
+VALUES 
+('Raju'),
+('Sham'),
+('Paul');
+
+INSERT INTO products (p_name, price, stock_quantity)
+VALUES
+('Laptop', 55000, 5),
+('Mouse', 500, 10),
+('Keyboard', 800, 7),
+('Cable', 250, 20);
